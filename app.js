b0a43102cae80cdf8722a02346e9c488969fc219
Shivam kumar singh <shivamkrsingh4688@gmail.com>
2022-12-06 23:21:47 +0530

    Backend

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const app = express();
 const router = require("./routes/book-routes");
 const cors = require("cors");
 
+const path = require("path");
+require('dotenv').config({path: './config.env'});
+
+
+
 app.use(express.json());
 app.use(cors());
 app.use("/books", router);
@@ -13,12 +18,28 @@ app.use("/books", router);
 //     app.listen(5000);
 // }).catch((err) =>console.log(err));
 
+
+
+const port = process.env.PORT || 5000;
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname + "/client/build/index.html"),
+      function (err) {
+        if (err) {
+          console.log(err);
+        }
+      }
+    );
+  });
+}
 mongoose
   .connect(
-    "mongodb+srv://admin:ogMUXixqITk2lerg@cluster0.ut3oc8b.mongodb.net/?retryWrites=true&w=majority"
+   process.env.URL
   )
   .then(() => console.log("Connected to the Database"))
   .then(() => {
-    app.listen(5000);
+    app.listen(port);
   })
   .catch((err) => console.log(err));
